9c04a9b86b43544528ca0b3f471d06265ddd7673
Matt Wiener <mwiener@outlook.com>
2018-06-03 07:34:15 -0400

    initial

--- HieghtWidth/Program.cs
+++ HieghtWidth/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HieghtWidth
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var x = 0;
+            do
+            {
+                Console.WriteLine("please enter the width of the image");
+                var width = Console.ReadLine();
+
+                if (!int.TryParse(width, out x)) Console.WriteLine("You did not enter a valid width");
+            } while (x == 0);
+
+            var y = 0;
+            do
+            {
+                Console.WriteLine("please enter the hieght of the image");
+                var width = Console.ReadLine();
+
+                if (!int.TryParse(width, out y)) Console.WriteLine("You did not enter a valid hieght");
+            } while (y == 0);
+
+            if (x > y)
+                Console.WriteLine("The image is in landscape");
+            else if (x == y)
+                Console.WriteLine("The image is a square");
+
+            else
+                Console.WriteLine("The image is in portrait");
         }
     }
 }
